857a9392718e7e065ed915549d31318d1237fd74
TrueMajner <49881015+TrueMajner@users.noreply.github.com>
2019-12-07 08:52:30 +0300

    Delete space.js

--- space.js
+++
@@ -1,6 +0,0 @@
-//Для получения аргументов команды с учётом того, что человек может писать по много пробелов между словами.
-function space(text){
-return text.replace(/\s+/g," ");
-}
-let str = "sadas       dssf sd    df df  dfdfd";
-let args = space(str.split(" "));
